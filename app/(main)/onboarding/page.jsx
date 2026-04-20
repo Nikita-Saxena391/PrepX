4a30c74b61934e2d82c846649881438f509a8725
Nikita Saxena <nikitasaxena.net@gmail.com>
2026-04-21 00:02:42 +0530

    Update page.jsx

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -2,12 +2,8 @@ import { redirect } from "next/navigation";
 import { industries } from "@/data/industries";
 import OnboardingForm from "./_components/onboarding-form";
 import { getUserOnboardingStatus } from "@/actions/user";
-import { checkUser } from "@/lib/checkUser"; // 👈 ADD THIS
 
 export default async function OnboardingPage() {
-  // ✅ ENSURE USER EXISTS IN DB
-  await checkUser();
-
   // Check if user is already onboarded
   const { isOnboarded } = await getUserOnboardingStatus();
 
